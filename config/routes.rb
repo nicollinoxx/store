bf4ab42b43a4ee59e79624f2b8b6388181fd2557
nicollinoxx <nicolasalmeida321@gmail.com>
2025-01-19 17:26:18 -0300

    Add unsubscribe to stock

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :products do
     resources :subscribers, only: [ :create ]
   end
+  
+  resource  :unsubscribe, only: [ :show ]
 
   root "products#index"
 end
