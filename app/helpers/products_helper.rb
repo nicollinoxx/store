28cc09840eb13e83dc69865db7e88ea6f4ec8824
Nicolas <nicolasalmeida321@gmail.com>
2025-02-11 13:38:50 -0300

    add helper to product views

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,5 +1,9 @@
 module ProductsHelper
   def review_exists?
-    ! @product.reviews.exists?(user_id: Current.user.id)
+    ! @product.reviews.exists?(user_id: Current.user.id) if authenticated?
+  end
+
+  def is_owner_review?(review)
+    review.user_id.equal?(Current.user.id) if authenticated?
   end
 end
