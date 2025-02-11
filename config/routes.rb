94fe05c76209de6cb37f9a358edacc07c7d197f6
Nicolas <nicolasalmeida321@gmail.com>
2025-02-10 23:23:48 -0300

    Add nested route

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
 
   resources :products do
     resources :subscribers, only: [ :create ]
+    resources :reviews
   end
 
-  resources :reviews
+
 
   resource :unsubscribe, only: [ :show ]
 
