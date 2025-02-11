150174f4e9203dcd1e0086fbc3d4a46fa8fdea7c
Nicolas <nicolasalmeida321@gmail.com>
2025-02-10 22:22:03 -0300

    Add association

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   include Notifications
 
   has_many :subscribers, dependent: :destroy
+  has_many :reviews,     dependent: :destroy
 
   has_one_attached :featured_image
   has_rich_text    :description
